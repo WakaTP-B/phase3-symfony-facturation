5d2c2e9bd8757b01cac79977c05c06f5bc81a62d
WakaTP-B <courcelles.amaury@orange.fr>
2026-05-11 01:21:11 +0200

    profil done

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('company_name', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(message: 'Veuillez indiquer votre raison sociale'),
+                    new Length(max: 255),
+                ],
+            ])
+            ->add('siret', TextType::class, [
+                'label' => false,
+                'required' => false,
+                'constraints' => [
+                    new Length(exactly: 14, exactMessage: 'Le SIRET doit contenir exactement 14 chiffres'),
+                ],
+            ])
+            ->add('iban', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank(message: 'Veuillez indiquer votre IBAN'),
+                    new Length(max: 34),
+                ],
+            ])
+            ->add('cgv', TextareaType::class, [
+                'label' => false,
+                'required' => false,
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
